96774590b5e3e1b706ed77becd198931d9ff91a3
Abhishek Tirkey <abhishektirkey.code@gmail.com>
2020-11-18 03:04:43 +0530

    Updates

---
+++ precache-manifest.6829eec3fb4e5e2689e15a6b6235eea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40559b5a219a9aee27473adbabe45b59",
+    "url": "/react-playground-app/index.html"
+  },
+  {
+    "revision": "0955d5b85fe4ca5d6a37",
+    "url": "/react-playground-app/static/css/main.25b9a346.chunk.css"
+  },
+  {
+    "revision": "2e9386840b50bc975b57",
+    "url": "/react-playground-app/static/js/2.2f26738b.chunk.js"
+  },
+  {
+    "revision": "62246c9244aa0d9dc779005b7cc7c543",
+    "url": "/react-playground-app/static/js/2.2f26738b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0955d5b85fe4ca5d6a37",
+    "url": "/react-playground-app/static/js/main.a3cda01b.chunk.js"
+  },
+  {
+    "revision": "e6cd72523f62dc3ba77b",
+    "url": "/react-playground-app/static/js/runtime-main.fb696ea2.js"
+  }
+]);
